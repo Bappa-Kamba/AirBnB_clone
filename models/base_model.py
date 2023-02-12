62e8914f8a0edca4dd4404aaf93f961711ca4f4a
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-02-12 18:19:59 +0100

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,19 @@ class BaseModel():
         """
         Initialize attributes: random uuid, dates created/updated
         """
+        if kwargs:
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
+                else:
+                    setattr(self, key, val)
+
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
